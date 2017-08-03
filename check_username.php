4b2adeaa1b46e2b2ecedafe90978b53ee432f96b
zqqian <zqqian@vip.qq.com>
2017-08-03 14:55:04 +0800

    0.2.8.3

--- check_username.php
+++ check_username.php
@@ -1,3 +1,7 @@
+
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+</head>
 <?php
 /*
 PHP+AJAX 检查注册账号时用户名是否存在
@@ -5,17 +9,23 @@ PHP+AJAX 检查注册账号时用户名是否存在
 
 
 */
- if($_GET[id]){
-    sleep(1);// what use for ?
-    $conn=mysql_connect('localhost','roster','roster666');
-    mysql_select_db('roster',$conn);
-    $sql="SELECT * FROM `user` WHERE `userName`='$_GET[id]'";
-    $q=mysql_query($sql);
- 
-    if(is_array(mysql_fetch_row($q))){
-      echo "用户名已经存在"; 
-    }else{
-      echo "用户名可以使用"; 
-    }
- }  
+error_reporting(0);
+if($_GET['id'])
+{
+	sleep(1);//为了显示等待检测这么一个动态效果，所以这里等待一秒执行
+	include("mysql-connect.php");
+	
+	$sql = "select * from user where userName='$_GET[id]'";
+	$result = mysql_query($sql);
+	
+	//这里的echo输出的内容便是将要返回给之前byphp()这个函数的值
+	if(is_array(mysql_fetch_row($result)))
+	{
+		echo "用户名已经存在！";
+	}
+	else
+	{
+		echo " user name is available";
+	}
+}
 ?>
--- index2.php
+++ index2.php
@@ -12,9 +12,7 @@
     <script src="js/jquery-3.2.1.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/npm.js"></script>
-	<script src="https://use.fontawesome.com/2957044126.js"></script>/*add awesome font */
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-	
     <style>
         /*为适应低版本的浏览器而设置的*/
         header, section, footer, aside, nav, main, article, figure {display: block;}
@@ -46,7 +44,7 @@
                                              <button type="button" class="close" data-dismiss="modal" aria-hidden="true"> &times;</button>
                                               <center> <h4 class="modal-title" id="myModalLabel">登录</h4></center>
                                          </div>
-                                         <form action="login.php" method="post" id="loginForm">
+                                         <form action="login.php" method="post" id="loginForm" name="loginForm">
                                             <div class="modal-body">
                                                 <input type="text" class="form-control" id="userName" name="userName" placeholder="请输入用户名"><br>
                                                 <input type="password" class="form-control" id="password" name="password" placeholder="请输入密码"><br>
@@ -54,12 +52,12 @@
                                                 <center>
                                                 	<label><input type="checkbox" id="rememberPwd" name="rememberPwd"> 记住密码</label>
                                                     <a href="#" name="forgetPwd">忘记密码</a>
-                                                </center>   
+                                                </center>
                                             </div>
                                             <div class="modal-footer">
                                                 <center>
                                                 <button type="submit" class="btn btn-primary"style="width:80px;">登录</button>
-                                                <button type="button" class="btn btn-default" data-dismiss="modal"style="width:80px;">关闭</button>        
+                                                <button type="button" class="btn btn-default" data-dismiss="modal"style="width:80px;">关闭</button>
                                                 </center>
                                             </div>
                                           </form>
@@ -72,16 +70,18 @@
                             <!-- 模态框（Modal） -->
                             <div class="modal fade" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel1" aria-hidden="true">
                                 <div class="modal-dialog">
-                                    <div class="modal-content">
+                                    <div class="modal-content" id="modal-content">
                                         <div class="modal-header">
                                             <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                                             <center><h4 class="modal-title" id="myModalLabel1">注册</h4></center>  
                                         </div>            
-                                        <form action="register.php" method="post" id="registerForm">        
-                                          <div class="modal-body">        
+
+
+                                          <div class="modal-body" id="modal-body">
+                                              <center><h1 id="registerShow">显示注册信息</h1></center>
                                           <input type="text" class="form-control" id="RuserName" name="RuserName" placeholder="请输入用户名"><br>
-                                       <div class="input-group">    
-                                          <span class="input-group-addon"></span>  </div>
+                                          <!--<div class="input-group">    
+                                          <span class="input-group-addon">@</span>-->    
                                           <input type="email" class="form-control" placeholder="请输入邮箱"
                                                  id="email" name="email" style="margin-bottom:20px;">
                                           <!--  </div>-->    
@@ -92,14 +92,16 @@
                                           <span class="help-block" id="registerSpan" style="color:red;">*除学校学院，其余都为必填项</span>
                                            <!--这里是帮助文本-->
                                           </div>
-                                          <div class="modal-footer">
+                                          <div class="modal-footer" id="modal-footer">
                                               <center>
-                                              <button type="submit" class="btn btn-primary" style="width:80px;">注册</button>
+                                              <button type="button" id="registerBtn"class="btn btn-primary" style="width:80px;">注册</button>
                                               <button type="button" class="btn btn-default" data-dismiss="modal"style="width:80px;">关闭
                                               </button>
                                               </center>           
                                           </div>
-                                         </form>
+
+
+
                                      </div><!-- /.modal-content -->
                                   </div>
 							 </div><!-- /.modal -->
@@ -139,10 +141,9 @@
             <div class="row" >
                 <div class="col-md-4">
                     <div class="thumbnail">
-					
-                        <i class="fa fa-qrcode fa-5x" aria-hidden="true"></i>
+                        <img alt="300x200" src="img/sign1.jpg" width="200px;" />
                         <div class="caption">
-                             <center>
+                            <center>
                             <h3>扫码签到</h3>              
                             </center>
                             <p>&nbsp;&nbsp;用户登录网站，选择点名功能，产生签到二维码，
@@ -153,9 +154,7 @@
                 </div>
                 <div class="col-md-4">
                     <div class="thumbnail">
-                      
-			<i class="fa fa-pencil-square-o fa-5x"></i> 		 
-						
+                        <img alt="300x200" src="img/sign2.jpg" width="200px;" />
                         <div class="caption">
                             <center>
                             <h3>便捷录入</h3>  
@@ -166,7 +165,8 @@
                     </div>
                 </div>
                 <div class="col-md-4">
-                    <div class="thumbnail"><i class="fa fa-bar-chart fa-5x"></i> 
+                    <div class="thumbnail">
+                        <img alt="300x200" src="img/sign3.jpg" width="200px;" />
                         <div class="caption">
                            <center>
                             <h3>智能分析</h3>  
@@ -192,12 +192,16 @@
 
 
 <script>
-/*for check password*/
-
 $(function(){
 	/*点击超链接时清空对应模态框的提示信息*/
 	$("#loginA").click(function(){$("#loginSpan").html("");});
-	$("#registerA").click(function(){$("#registerSpan").html("*除学校学院，其余都为必填项");});
+	$("#registerA").click(function(){
+        $("#modal-body").children().show();
+        $("#registerShow").hide();
+        $("#modal-footer").show();
+        $("#registerSpan").html("*除学校学院，其余都为必填项");
+
+    });
 		
 	$("#loginForm").submit(function(event){
 		var username=$("#userName").val();
@@ -208,69 +212,57 @@ $(function(){
 			if(""==pwd)		    {event.preventDefault();$("#loginSpan").html("*请输入密码");}
 		}	
 	});
-	$("#registerForm").submit(function(event){
+
+    $("#registerBtn").click(function(event){
+        var flag=false;
+        var username=$("#RuserName").val();
+        var email=$("#email").val();
+        var pwd1=$("#Rpassword1").val();
+        var pwd2=$("#Rpassword2").val();
+        var school=$("#Rschool").val();
+        var academy=$("#Racademy").val();
+        //event.preventDefault();alert(username+"*"+email+pwd1+"*"+pwd2+"*"+school+"*"+academy+"*");
+        if(""==username) {event.preventDefault();$("#registerSpan").html("*请输入用户名");$("#RuserName").focus();}
+        else if(""==email){event.preventDefault();$("#registerSpan").html("*请输入邮箱");$("#email").focus();}
+        else if(""==pwd1) {event.preventDefault();$("#registerSpan").html("*请输入密码");$("#Rpassword1").focus();}
+        else if(pwd1.length<6){event.preventDefault();$("#registerSpan").html("*密码不能低于6位");$("#Rpassword1").focus();}
+        else if(""==pwd2) {event.preventDefault();$("#registerSpan").html("*请重复密码");$("#Rpassword2").focus();}
+        else if(pwd2.length<6){event.preventDefault();$("#registerSpan").html("*密码不能低于6位");$("#Rpassword2").focus();}
+        else if(pwd1!=pwd2){event.preventDefault();$("#registerSpan").html("*两次输入的密码不一致");$("#Rpassword1").focus();}
+        else{ flag=true;}
+
+
+        if(flag){
+            $.post("register.php",{},function(data){
+
+                $("#modal-body").find(":input").hide();
+                $("#modal-body").find("span").hide();
+                $("#registerShow").show();
+                $("#modal-footer").hide();
+
+            });
+        }
+
+    });
+});
+    /*$("#registerForm").submit(function(event){
 		var username=$("#RuserName").val();
 		var email=$("#email").val();
 		var pwd1=$("#Rpassword1").val();
 		var pwd2=$("#Rpassword2").val();
 		var school=$("#Rschool").val();
 		var academy=$("#Racademy").val();
-		
-       
-		/*event.preventDefault();alert(username+"*"+email+pwd1+"*"+pwd2+"*"+school+"*"+academy+"*");*/
+		//event.preventDefault();alert(username+"*"+email+pwd1+"*"+pwd2+"*"+school+"*"+academy+"*");
 		if(""==username) {event.preventDefault();$("#registerSpan").html("*请输入用户名");$("#RuserName").focus();}
 		else if(""==email){event.preventDefault();$("#registerSpan").html("*请输入邮箱");$("#email").focus();}
 		else if(""==pwd1) {event.preventDefault();$("#registerSpan").html("*请输入密码");$("#Rpassword1").focus();}
 		else if(pwd1.length<6){event.preventDefault();$("#registerSpan").html("*密码不能低于6位");$("#Rpassword1").focus();}
 		else if(""==pwd2) {event.preventDefault();$("#registerSpan").html("*请重复密码");$("#Rpassword2").focus();}
+        else if(pwd2.length<6){event.preventDefault();$("#registerSpan").html("*密码不能低于6位");$("#Rpassword2").focus();}
 		else if(pwd1!=pwd2){event.preventDefault();$("#registerSpan").html("*两次输入的密码不一致");$("#Rpassword1").focus();}
 		else{}		
 		});
-	});
-</script>	
-	
-<script>
-	var xmlHttp;  
-    var useridObj=document.getElementById("RuserName");  
-    useridObj.onblur=checkUserid;//
-  function S_xmlhttprequest()  
-    {  
-        if(window.ActiveXObject)  
-        {  
-            xmlHttp = new ActiveXObject('Microsoft.XMLHTTP');  
-        }  
-        else if(window.XMLHttpRequest)  
-        {  
-            xmlHttp = new XMLHttpRequest();  
-        }  
-    }  
-
-	function checkUserid(){  
-			var useridValue = trim(useridObj.value); 
-			var span = document.getElementById("input-group-addon");  
-			S_xmlhttprequest();  
-            //这里是异步运行了，当js执行到这一句话后不会等待他的返回值，而是直接往下进行，我测试出来的效果是当你js代码执行完了这里的值才返回来。  
-            xmlHttp.open("POST","check_username.php?id="+useridValue,true);//这个页面便是你要进行选择查询的PHP页面  
-            xmlHttp.onreadystatechange = byphp;//接受返回值  
-            xmlHttp.send(null);
-	
-	}
-	
-	  function byphp()  
-    {  
-        var msg;  
-    if(xmlHttp.readyState==1)//等于1是指还未获取返回值的意思  
-    {  
-        document.getElementById('input-group-addon').innerHTML = "checking";  
-    }  
-    else  
-    {  
-        var byphp100 = xmlHttp.responseText;//接受PHP的返回值       
-        document.getElementById('input-group-addon').innerHTML = byphp100; //设置span里的内容  
-    }  
-}  
-	
-	
+	});*/
 	
 </script>
 </body>
