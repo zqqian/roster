3556c7656ecc821e9a34eea42747a173bd5a4ec7
foodieLu <30677780+foodieLu@users.noreply.github.com>
2017-08-08 23:21:03 +0800

    修改代码
    原来的代码显示在PHPstorm上有删除线，强迫症改掉了，代码与原意相同

--- mysql-connect.php
+++ mysql-connect.php
@@ -1,14 +1,6 @@
 <?php
 // 连接服务器，并且选择roster数据库
 
-	
-$db = mysql_connect("localhost","roster","roster666") 
-	or die("连接数据库失败！");
+$db = mysqli_connect("localhost","roster","roster666","roster") or die("连接数据库失败！");
 
-mysql_select_db("roster")
-	or die ("不能连接到user".mysql_error());
-	
-	
-
-	
 ?>
