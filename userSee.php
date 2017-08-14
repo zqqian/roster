7f1d52bae95886aa6b34885ae4738e32388327d6
foodieLu <30677780+foodieLu@users.noreply.github.com>
2017-08-14 12:46:27 +0800

    修改bug
    1.谷歌浏览器不兼容
    2.页面重复问题
    3.导出页面链接

--- userSee.php
+++ userSee.php
@@ -1,13 +1,13 @@
-
 <?php
- require_once 'get_user_info.php';
+/*
+//为方便本页面测试 暂时屏蔽
+require_once 'get_user_info.php';
 
 if(!$is_login){
 	echo "<script> alert('Please login...');parent.location.href='./index.php'; </script>"; 
-}
+}*/
 ?>
 <!doctype html>
-
 <html lang="en">
 <head>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
@@ -25,14 +25,16 @@ if(!$is_login){
         /*为适应低版本的浏览器而设置的*/
         header, section, footer, aside, nav, main, article, figure {display: block;}
         html,body{margin:0;}
+        .row{margin:0;}
         .container{
             width: 100%;
             margin: 60px 0 0 0;
             height:500px;
+            padding: 0;
             }
 
         #left {
-            width: 12%;
+            width: 180px;
             min-width: 180px;
             min-height: 420px;
             position: absolute;
@@ -40,7 +42,6 @@ if(!$is_login){
         }
 
         #right {
-            width: -moz-available;
             height: 100%;
             padding: 0;
             min-width: 350px;
@@ -48,14 +49,23 @@ if(!$is_login){
             position: absolute;
             left: 180px;
         }
+
+        iframe{
+            width:99.9%;
+            height:99%;
+            float:right;
+            display:inline-block;
+            margin:0 auto;
+        }
     </style>
 </head>
 <body>
 <div class="container" id="container">
     <div class="row clearfix">
-        <div class="col-md-12 column">
+
+        <div class="col-md-12 column" id="col12">
             <!--导航栏开始-->
-            <nav class="navbar navbar-default navbar-fixed-top navbar-inverse" role="navigation">
+            <nav class="navbar navbar-default navbar-fixed-top navbar-inverse" role="navigation" id="daohang">
                 <div class="navbar-header">
                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                         <span class="sr-only">Toggle navigation</span>
@@ -67,13 +77,9 @@ if(!$is_login){
                 </div>
 
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-
-
                     <ul class="nav navbar-nav navbar-right">
                         <li>
-
-                            <a href="#" ><?php  echo $username; ?></a>
-
+                            <a><?php $output = isset($username) ? $username : '未登录测试'; echo $output; /*echo $output;*/?></a>
                         </li>
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" style="margin-right: 50px;">用户设置<strong class="caret"></strong></a>
@@ -84,7 +90,9 @@ if(!$is_login){
                                 <li>
                                     <a href="repassword.html" target="rightShow">修改用户密码</a>
                                 </li>
-
+                                <li>
+                                    <a href="#" id="logout">退出</a>
+                                </li>
                             </ul>
                         </li>
                     </ul>
@@ -92,13 +100,10 @@ if(!$is_login){
             </nav>
             <!--导航栏结束-->
 
-
-
              <!-- <div class="row clearfix" id="mainContainer"></div>-->
                     <div class="col-md-6 column" id="left">
                         <!--手风琴切换开始-->
                         <div class="panel-group" id="menu" style="margin-top:20px;">
-
                             <!-- 导入/导出 -->
                             <div class="panel panel-default">
                                 <div class="panel-heading">
@@ -110,7 +115,7 @@ if(!$is_login){
                                         <a href="import.php" target="rightShow" id="import">导入学生名单</a>
                                     </div>
                                     <div class="panel-body">
-                                        <a href="#" target="" id="export">导出学生成绩</a>
+                                        <a href="export.php" target="rightShow" id="export">导出学生成绩</a>
                                     </div>
                                 </div>
                             </div>
@@ -124,18 +129,17 @@ if(!$is_login){
                                 </div>
                                 <div id="panel2" class="panel-collapse collapse collapse">
                                     <div class="panel-body">
-                                        <a href="#" target="" id="import">自动点名</a>
+                                        <a href="#" target="rightShow" id="import">自动点名</a>
                                     </div>
                                     <div class="panel-body">
                                         <a href="manuallcall.php" target="rightShow" id="import">手动点名</a>
                                     </div>
                                     <div class="panel-body">
-                                        <a href="#" target="" id="export">点名信息查询</a>
+                                        <a href="#" target="rightShow" id="export">点名信息查询</a>
                                     </div>
                                 </div>
                             </div>
                             <!-- 点名 -->
-
 
                             <!-- 成绩 -->
                             <div class="panel panel-default">
@@ -145,10 +149,10 @@ if(!$is_login){
                                 </div>
                                 <div id="panel3" class="panel-collapse collapse collapse">
                                     <div class="panel-body">
-                                        <a href="#" target="" id="import">成绩录入</a>
+                                        <a href="#" target="rightShow" id="import">成绩录入</a>
                                     </div>
                                     <div class="panel-body">
-                                        <a href="#" target="" id="import">成绩查询</a>
+                                        <a href="#" target="rightShow" id="import">成绩查询</a>
                                     </div>
 
                                 </div>
@@ -158,8 +162,8 @@ if(!$is_login){
                             <!-- 数据分析 -->
                             <div class="panel panel-default">
                                 <div class="panel-heading">
-                                    <a class="panel-title" data-toggle="collapse"
-                                       data-parent="#menu">数据分析</a>
+                                    <a class="panel-title" href="#"
+                                       data-parent="#menu" target="rightShow">数据分析</a>
                                 </div>
                             </div>
                             <!-- 数据分析 -->
@@ -177,197 +181,11 @@ if(!$is_login){
                         </div>
                         <!--手风琴切换结束-->
 
-
-
-
                     </div>
                      <div class="col-md-6 column" id="right">
-                         <iframe src="https://www.baidu.com/" name="rightShow" style="width:99.9%;height:99%;float:right;display:inline-block;margin:0 auto;"
-                                   frameborder="no" align="center" seamless>
+                         <iframe src="https://www.runoob.com/" name="rightShow" frameborder="no" align="center" seamless>
                              <p>您的浏览器不支持iframe 标签。</p>
                          </iframe>
-
-
-                     </div>
-        </div><!doctype html>
-<html lang="en">
-<head>
-    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-    <meta name="keywords" content="云点名系统">
-    <meta name="description" content="云点名系统">
-    <meta name="author" content="创意武装H5团队">
-    <!--每30秒中刷新当前页面:<meta http-equiv="refresh" content="30">-->
-
-    <title>云点名用户界面</title>
-    <script src="js/jquery-3.2.1.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/npm.js"></script>
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    <style>
-        /*为适应低版本的浏览器而设置的*/
-        header, section, footer, aside, nav, main, article, figure {display: block;}
-        html,body{margin:0;}
-        .container{
-            width: 100%;
-            margin: 60px 0 0 0;
-            height:500px;
-            }
-
-        #left {
-            width: 12%;
-            min-width: 180px;
-            min-height: 420px;
-            position: absolute;
-            left: 0;
-        }
-
-        #right {
-            width: -moz-available;
-            height: 100%;
-            padding: 0;
-            min-width: 350px;
-            min-height: 420px;
-            position: absolute;
-            left: 180px;
-        }
-    </style>
-</head>
-<body>
-<div class="container" id="container">
-    <div class="row clearfix">
-        <div class="col-md-12 column">
-            <!--导航栏开始-->
-            <nav class="navbar navbar-default navbar-fixed-top navbar-inverse" role="navigation">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="userSee.php">云点名系统</a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-
-
-                    <ul class="nav navbar-nav navbar-right">
-                        <li>
-                            <a href="#" id="logout" ><?php  echo $username; ?></a>
-                        </li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" style="margin-right: 50px;">用户设置<strong class="caret"></strong></a>
-                            <ul class="dropdown-menu">
-                                <li>
-                                    <a href="information.html" target="rightShow">修改用户信息</a>
-                                </li>
-                                <li>
-                                    <a href="repassword.html" target="rightShow">修改用户密码</a>
-                                </li>
-
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-            <!--导航栏结束-->
-
-
-
-             <!-- <div class="row clearfix" id="mainContainer"></div>-->
-                    <div class="col-md-6 column" id="left">
-                        <!--手风琴切换开始-->
-                        <div class="panel-group" id="menu" style="margin-top:20px;">
-
-                            <!-- 导入/导出 -->
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <a class="panel-title" data-toggle="collapse"
-                                       data-parent="#menu" href="#panel1">导入/导出</a>
-                                </div>
-                                <div id="panel1" class="panel-collapse collapse collapse"><!--panel-collapse collapse in 表示默认面板打开-->
-                                    <div class="panel-body">
-                                        <a href="import.php" target="rightShow" id="import">导入学生名单</a>
-                                    </div>
-                                    <div class="panel-body">
-                                        <a href="#" target="" id="export">导出学生成绩</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- 导入/导出 -->
-
-                            <!-- 点名 -->
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <a class="panel-title" data-toggle="collapse"
-                                       data-parent="#menu" href="#panel2">点名</a>
-                                </div>
-                                <div id="panel2" class="panel-collapse collapse collapse">
-                                    <div class="panel-body">
-                                        <a href="#" target="" id="import">自动点名</a>
-                                    </div>
-                                    <div class="panel-body">
-                                        <a href="manuallcall.php" target="rightShow" id="import">手动点名</a>
-                                    </div>
-                                    <div class="panel-body">
-                                        <a href="#" target="" id="export">点名信息查询</a>
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- 点名 -->
-
-
-                            <!-- 成绩 -->
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <a class="panel-title" data-toggle="collapse"
-                                       data-parent="#menu" href="#panel3">成绩</a>
-                                </div>
-                                <div id="panel3" class="panel-collapse collapse collapse">
-                                    <div class="panel-body">
-                                        <a href="#" target="" id="import">成绩录入</a>
-                                    </div>
-                                    <div class="panel-body">
-                                        <a href="#" target="" id="import">成绩查询</a>
-                                    </div>
-
-                                </div>
-                            </div>
-                            <!-- 成绩 -->
-
-                            <!-- 数据分析 -->
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <a class="panel-title" data-toggle="collapse"
-                                       data-parent="#menu">数据分析</a>
-                                </div>
-                            </div>
-                            <!-- 数据分析 -->
-
-                            <!-- 帮助 -->
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <a  href="helpfile.html" class="panel-title"
-                                       data-parent="#menu" target="rightShow">帮助</a>
-                                </div>
-
-                            </div>
-                            <!-- 帮助 -->
-
-                        </div>
-                        <!--手风琴切换结束-->
-
-
-
-
-                    </div>
-                     <div class="col-md-6 column" id="right">
-                         <iframe src="https://www.baidu.com/" name="rightShow" style="width:99.9%;height:99%;float:right;display:inline-block;margin:0 auto;"
-                                   frameborder="no" align="center" seamless>
-                             <p>您的浏览器不支持iframe 标签。</p>
-                         </iframe>
-
-
                      </div>
         </div>
     </div>
@@ -378,7 +196,7 @@ if(!$is_login){
             if(msg==1)
             {
                 alert("退出成功!")
-//                返回到首页
+               // 返回到首页
                 window.location.href='../index/index.html';//首页路径
             }
         })
@@ -387,59 +205,34 @@ if(!$is_login){
     function auto(){
         //container的自适应
         var bodyHeight=document.documentElement.clientHeight;
+        var bodyWidth=document.documentElement.clientWidth;
+
         $("#container").height(bodyHeight-60);
+        $("#container").width(bodyWidth);
+
+
+
 
         //left和right的自适应
-        var temp=$("#container").height();
+        var tempH=$("#container").height();
+        var tempW=$("#container").width();
 
-        if(temp>420){
-            $("#left").height(temp);
-            $("#right").height(temp);
+
+        if(tempH>420){
+            $("#left").height(tempH);
+            $("#right").height(tempH);
         }
         else
         {   $("#left").height(420);
             $("#right").height(420);}
-    }
 
-    $(window).resize(function(){
-       auto();
-    });
+        if(tempW>420){
 
-    $(function(){
-      auto();
-    });
-</script>
-</body>
-</html>
-    </div>
-</div>
-<script>
-    $("#logout").click(function(){
-        $.post("logout.php?action=logout",function(msg){
-            if(msg==1)
-            {
-                alert("退出成功!")
-//                返回到首页
-                window.location.href='../index/index.html';//首页路径
-            }
-        })
-    })
-
-    function auto(){
-        //container的自适应
-        var bodyHeight=document.documentElement.clientHeight;
-        $("#container").height(bodyHeight-60);
-
-        //left和right的自适应
-        var temp=$("#container").height();
-
-        if(temp>420){
-            $("#left").height(temp);
-            $("#right").height(temp);
+            $("#right").width(tempW-180);
         }
         else
-        {   $("#left").height(420);
-            $("#right").height(420);}
+        {
+            $("#right").width(420);}
     }
 
     $(window).resize(function(){
