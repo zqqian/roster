1cfd1bf889a069073942c8dc304b4fa1d15a5dc1
yixue66 <2382844503@qq.com>
2017-08-22 10:57:33 +0800

    点名信息查询修正

--- attendance.php
+++ attendance.php
@@ -1,4 +1,11 @@
-<!DOCTYPE html>
+<?php
+require_once 'get_user_info.php';
+//用于检测是否登录，测试本页面时可暂时屏蔽以下几行php代码
+if(!$is_login){
+    echo "<script> alert('Please login...');parent.location.href='./index.php'; </script>";
+}
+?>
+<!doctype html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -6,16 +13,35 @@
     <script src="../js/jquery-3.2.1.js"></script>
 </head>
 <style>
-    html,body{margin:0px;}
-    #menuselect{display:block;width:100%;height:50px;background-color:#212121;
-        text-align:center;margin: 0px;padding:0px;line-height:75px;}
+    html,body{margin:0px;padding:0px;}
+    #course_name{display:block;width:100%;height:50px;background-color:#000000;text-align:center;line-height:80px;}
+    #course_name span{font-size:25px;color:#f9f9f9;margin-right:25%;position:absolute;}
+    #select_course{margin:20px;height:25px;text-align:center;margin-top:20px;margin-left:160px;}
+    #menuselect{display:block;width:100%;height:60px;margin: 0px;background-color:#000000;
+        text-align:center;padding:0px;line-height:85px;}
+    #menuselect ul{background-color:#000000;margin:0px;display:block;padding:0px;}
     .showa{font-size: 20px;color: #fffdfc;}
     ul li{list-style-type:none;display:inline;margin:7%;}
-    #menusure{display:block;width:100%;height:100px;background-color:#E6F5FF;text-align:center;line-height:100px;}
-    span{font-size:18px;}
-    select{margin:10px;}
+    #menusure{display:block;width:100%;height:80px;background-color:#E6F5FF;text-align:center;line-height:80px;}
+    #menusure span{font-size:18px;}
+    #select-class{margin:10px;}
 </style>
 <body>
+<div id="course_name">
+    <span>选择课程</span>
+    <select id="select_course" onChange = "getcourse()">
+        <option value="请选择" selected></option>
+        <?php
+        $userid = $_SESSION['userid'];
+        $find_class = "SELECT DISTINCT a.classId,className,enterYear FROM class as a,class_course_user as b where a.classId=b.classId and b.userId=".$userid;
+        require "mysql-connect.php";
+        $set=mysqli_query($db,$find_class);
+        while($row=mysqli_fetch_assoc($set)){
+            echo "<option value='".$row['course_name']."</option>";
+        }
+        mysqli_close($db);?>
+    </select>
+</div>
 <div id="menuselect">
     <ul>
         <li> <a href="#" class="showa" id="showaone" onclick="searchdate()">按日期查询</a></li>
@@ -37,62 +63,66 @@
     var userId=session.getAttribute("userID").toString(); //返回班级名称字符串，以,隔开
     var date_array=new Array();
     var class_array=new Array();
-    $(document).ready(function(){
+    function getcourse(){
+        var obj = document.getElementById("select_course");
+        var index = obj.selectedIndex;
+        if (obj.options[index].value != "请选择") {
         $.ajax({
             type:"POST",
             url:"attendance.php",
-            data:{userId:userId},
+            data:{userId:<?php echo $_SESSION['userid'];?>},selected_course:obj.options[index].value}
             dataType:"json",
             success:function(data) {   //返回一个json数组，数组中分别包含班级和日期两个数组
                 date_array=data.search_date;
                 class_array=data.search_class;
             }
         })
-    })
+    }
+    }
     function searchdate()
     {
-        $("span").html("");
-        $("span").html("选择日期");
+        $("#menusure span").html("");
+        $("#menusure span").html("选择日期");
         $("#select-class").html("");
         for(var i=0;i<date_array.length;i++)
         {
             $("#select-class").append("<opotion value='"+date_array[i]+"'>"+date_array[i]+"</opotion>");
         }
-        $("#select-class").append("<opotion value=\"-1\">选择日期</opotion>");
+        $("#select-class").append("<opotion value=\"请选择\" selected>选择日期</opotion>");
     }
     function searchclass(){
-        $("span").html("");
-        $("span").html("选择班级");
+        $("#menusure span").html("");
+        $("#menusure span").html("选择班级");
         $("#select-class").html("");
         for(var i=0;i<class_array.length;i++)
         {
             $("#select-class").append("<opotion value='"+class_array[i]+"'>"+class_array[i]+"</opotion>");
         }
-        $("#select-class").append("<opotion value=\"-1\">选择班级</opotion>");
+        $("#select-class").append("<opotion value=\"请选择\" selected>选择班级</opotion>");
     }
     function attendance_data() {
-        var search = $("span").html();  //获取当前标签值
+        var search = $("#menusure span").html();  //获取当前标签值
         var obj = document.getElementById("select_class");
         var index = obj.selectedIndex;
         if (search = "选择日期") {
-            if (obj.options[index].value != -1) {
+            if (obj.options[index].value !="请选择") {
                 $.ajax({
                     type: "POST",
                     url: "attendance.php",
-                    data: {search: search, select_class: obj.options[index].value},
+                    data: {search: search, select_class: obj.options[index].value,userId:<?php echo $_SESSION['userid'];?>},selected_course:obj.options[index].value},
                     dataType: "json",
                     success: function (data) {   //返回数据为班级名、其具体点名时间和缺勤绿
                         var h = "<table id=tb>";
                         for (var i = 0; i < data.length; i++) {
                             h += "<tr>";
-                            h += "<td>" + data[i].class_name + "</td>" + "<td>" + data[i].call_time + "</td>" +
-                                    "<td>" + data[i].attendance_rate_ave + "</td>";
+                            h += "<td>" + data[i].class_name + "</td>" + "<td>" + data[i].call_time + "</td>" + "<td>" + data[i].attendance_rate_ave + "</td>";
                             h += "</tr>"
                         }
                         h += "</table>";
                         document.getElementById("showdata").innerHTML = h;
                     }
                 })
+
             }
         }
         else if (search = "选择班级") {
@@ -100,31 +130,22 @@
                 $.ajax({
                     type: "POST",
                     url: "attendance.php",
-                    data: {search: search, select_class: obj.options[index].value},
+                    data: {search: search, select_class: obj.options[index].value,userId:<?php echo $_SESSION['userid'];?>},selected_course:obj.options[index].value},
                     dataType: "json",
                     success: function (data) {    //返回数据为学号，学生，点名几次，实到几次，总缺勤率
-<<<<<<< HEAD:attendance.html
                         var h="";
                         if(data.length>0) {
                             h+= "<table id=tb>";
                             for (var i = 0; i < data.length; i++) {
                                 h += "<tr>";
                                 h += "<td>" + data[i].student_number + "</td>" + "<td>" + data[i].student_name + "</td>" + "<td>" + data[i].call_number + "</td>" +
-                                        "<td>" + data[i].come_number + "</td>" + data[i].attendance_rate_sum + "</td>";
+                                    "<td>" + data[i].come_number + "</td>" + data[i].attendance_rate_sum + "</td>";
                                 h += "</tr>"
                             }
                             h += "</table>";
                         }
                         else{
-                              h="<span>+该班未点过名</span>";
-=======
-                        var h = "<table id=tb>";
-                        for (var i = 0; i < data.length; i++) {
-                            h += "<tr>";
-                            h += "<td>" + data[i].student_number + "</td>" + "<td>" + data[i].student_name + "</td>" + "<td>" + data[i].call_number + "</td>" +
-                                "<td>" + data[i].come_number + "</td>" + data[i].attendance_rate_sum + "</td>";
-                            h += "</tr>"
->>>>>>> c7a7f11b8a84ed563b9355388833dcc3c45851dd:attendance.php
+                            h="<span>+该班该课程未点过名</span>";
                         }
                         document.getElementById("showdata").innerHTML = h;
                     }
@@ -134,5 +155,6 @@
         else {
         }
     }
+
 </script>
 </html>
