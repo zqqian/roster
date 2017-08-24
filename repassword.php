d27c8d02e980fde46c07e1a51cbf0ce16bedc1c1
zqqian <zqqian@vip.qq.com>
2017-08-24 22:09:09 +0800

    1

---
+++ repassword.php
@@ -0,0 +1,32 @@
+<?php
+require_once "get_user_info.php";
+
+$oldpassword=$_POST['oldpassword'];
+$newpassword=$_POST['newpassword'];
+$newpassword2=$_POST['newpassword2'];
+
+$pw3=substr(md5(md5(md5($$oldpassword)).md5($username)),0,20);
+		    $sql = "select passWord from user where userName = '$username' ";
+            $result=mysqli_query($db,$sql);
+			$row = mysqli_fetch_assoc($result);
+			
+            if($row['passWord']==$pw3)  
+            {
+				if($newpassword==""||$newpassword2==""||$newpassword!=$newpassword2){
+					echo "0";
+				}else{
+					$npw=substr(md5(md5(md5($$oldpassword)).md5($username)),0,20);
+					$sql = "UPDATE `roster`.`user` SET `passWord` = $npw' WHERE `user`.`userId` = $userid";
+					$result=mysqli_query($db,$sql_insert);
+					if(!$result) echo"0";//Ê§°Ü
+					else         echo"1";
+				}
+
+				//echo "q";/*"<script> alert('Login successfully');parent.location.href='./userSee.php'; </script>"; */
+            }  
+            else  
+            {  
+		
+               echo "0";/*"wrong password !";*/
+		}
+?>
