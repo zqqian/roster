ff3db8d12a9228cc01eb05f0a9849f3ffdc9fa1e
zqqian <zqqian@vip.qq.com>
2017-08-25 22:46:51 +0800

    .

--- repassword.php
+++ repassword.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once "get_user_info.php";
-
+error_reporting(1);
 $oldpassword=$_POST['oldpassword'];
 $newpassword=$_POST['newpassword'];
 $newpassword2=$_POST['newpassword2'];
@@ -14,12 +14,14 @@ $pw3=substr(md5(md5(md5($oldpassword)).md5($username)),0,20);
             if($row['passWord']==$pw3)
             {
 				if($newpassword==""||$newpassword2==""||$newpassword!=$newpassword2){
-					echo "0";
+					echo "3";
 				}
                 else{
 					$npw=substr(md5(md5(md5($newpassword)).md5($username)),0,20);
-					$sql = "UPDATE `roster`.`user` SET `passWord` = $npw WHERE `user`.`userId` = $userid";
-					$result=mysqli_query($db,$sql_insert);
+					//echo $npw;
+					$sql = "UPDATE `user` SET `passWord` = '$npw' WHERE userName = '$username'";
+					$result=mysqli_query($db,$sql);
+					//var_dump($result);
 					if(!$result) echo"0";//ʧ��
 					else
                     {
