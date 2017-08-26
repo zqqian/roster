b1bab3c4deee71f46f59efa922a5664aba868bf7
zqqian <zqqian@vip.qq.com>
2017-08-26 12:09:55 +0800

    recovery change

--- creat_manuallcall_list.php
+++ creat_manuallcall_list.php
@@ -41,7 +41,48 @@ $sql = "SELECT DISTINCT `student`.`stuId` \n"
 $result=mysqli_query($db,$sql);
 $i=0;
 while($row[$i++] = mysqli_fetch_assoc($result));
-$randnum=rand(0,$i-1);
+// random algorithm
+$remain_i=$i;
+$a[$i];
+while($remain_i--){
+$randnum=rand(0,$remain_i-1);
+
+}
+$remain_i=$i;
+$a[$i];
+$b[$i];
+$iii=0;
+for($ii=0;$ii<$i;$ii++){
+	$a[$ii]=0;
+}
+while($remain_i--){
+	
+	
+$randnum=rand(1,$remain_i+1);
+//echo $randnum;
+$nowi=0;
+$nowii=0;
+//echo $nowii;
+while(1){
+	
+	if($a[$nowii]!=1){
+		
+		$nowi++;
+		
+	}
+	
+if($nowi==$randnum){
+	$a[$nowii]=1;
+	break;
+}$nowii++;	
+}
+
+$b[$iii++]=$nowii;
+
+
+}
+
+
 
 $randstuid=$row[$randnum]['stuId'];
 
