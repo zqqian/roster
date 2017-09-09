402f67eda828e936080e8c6b68b4e69cca1aa755
zqqian <zqqian@vip.qq.com>
2017-09-09 14:33:10 +0800

    update

--- data-analyse.php
+++ data-analyse.php
@@ -1,3 +1,4 @@
+
 <?php
 
 
@@ -13,6 +14,7 @@ if($kc==""&&$bj==""){
     . " FROM `class_course_user` WHERE `userId` = '$userid' LIMIT 0, 30 ";
 
 $result=mysqli_query($db,$sql);
+$i=0;
 if($result){
 	while($row = mysqli_fetch_assoc($result)){
 		$courseid=$row['courseId'];
@@ -23,16 +25,20 @@ if($result){
 		//var_dump($coursename);
 		$sql = "SELECT * FROM `class_course_user` WHERE `courseId` = '$courseid' AND `userId` = '$userid' LIMIT 0, 30 ";
 		$result2=mysqli_query($db,$sql);
-		$i=0;
+		$j=0;
+		$data[$i]['coursename']=$coursename;
 	while($row2= mysqli_fetch_assoc($result2)){
 		$classid=$row2['classId'];
 		//var_dump($classid);
 		$sql3 = "SELECT * FROM `class` WHERE `classId` = '$classid' LIMIT 0, 30 ";
 		$result3=mysqli_query($db,$sql3);
 		$row3= mysqli_fetch_assoc($result3);
-		$data[$coursename][$i++]=$row3['className'];
-	}
 		
+		//$data[$coursename][$i++]=$row3['className'];
+		$data[$i]['classname'][$j++]=$row3['className'];
+	
+	}
+	$i++;	
 		
 	}
 
