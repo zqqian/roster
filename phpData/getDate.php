26b7d554c2149d437d32667559a1fdd25546dc90
foodieLu <30677780+foodieLu@users.noreply.github.com>
2017-09-11 00:52:02 +0800

    导入导出除字体外都结束

--- phpData/getDate.php
+++ phpData/getDate.php
@@ -1,14 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: 卢
- * Date: 2017/8/21
- * Time: 9:41
- */
+
 $course = $_GET['course'];
 $class = $_GET['class'];
 $userId = $_GET['userId'];
 
+//$course = 39;
+//$class = 55;
+//$userId = 18;
 
 require "../mysql-connect.php";
 $find_id="select * from basic_relation where userId=$userId and courseId=$course and classId=$class";
@@ -16,7 +14,7 @@ $result=mysqli_query($db,$find_id);
 $row = mysqli_fetch_assoc($result);
 $Id=$row['Id'];
 
-$find_start_end = "SELECT Id,min(rosterDate) as min,max(rosterDate) as max FROM `classroster`  group by Id having Id=$userId";
+$find_start_end = "SELECT Id,min(rosterDate) as min,max(rosterDate) as max FROM `classroster`  group by Id having Id=$Id";
 $result=mysqli_query($db,$find_start_end);
 $row = mysqli_fetch_assoc($result);
 
